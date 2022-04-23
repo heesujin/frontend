71f14a5207615b15a5e48e0433cebd519f769574
junsu0121 <sayber123@naver.com>
2022-04-24 02:51:51 +0900

    수정3일차

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-app = Flask(__name__, static_folder='templates/assets')
+app = Flask(__name__)
 
 import certifi
 ca = certifi.where()
@@ -16,35 +16,6 @@ def home():
 def data_get():
     cafe_list = list(db.cafe.find({}, {'_id': False}))
     return jsonify({'cafe': cafe_list})
-#
-# @app.route("/bucket", methods=["POST"])
-# def bucket_post():
-#     bucket_receive = request.form['bucket_give']
-#
-#     bucket_list = list(db.bucket.find({}, {'_id': False}))
-#     count = len(bucket_list) + 1
-#
-#     doc = {
-#         'num':count,
-#         'bucket':bucket_receive,
-#         'done':0
-#     }
-#
-#     db.bucket.insert_one(doc)
-#
-#     return jsonify({'msg': '등록 완료!'})
-#
-# @app.route("/bucket/done", methods=["POST"])
-# def bucket_done():
-#     num_receive = request.form['num_give']
-#     db.bucket.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
-#     return jsonify({'msg': '버킷완료!'})
-#
-# @app.route("/bucket", methods=["GET"])
-# def bucket_get():
-#     bucket_list = list(db.bucket.find({}, {'_id': False}))
-#
-#     return jsonify({'buckets': bucket_list})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=7777, debug=True)
+    app.run('0.0.0.0', port=2006, debug=True)
